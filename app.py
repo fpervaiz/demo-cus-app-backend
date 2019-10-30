a243e6c3ef7e40463cff9d5f1c87de7c0aa8e920
Faizaan Pervaiz <fp361@cam.ac.uk>
2019-10-30 21:25:42 +0000

    Gunicorn update

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Build the SQLite URL for SqlAlchemy
 sqlite_url = "sqlite:///" + os.path.join(basedir, "data.db")
 
+
 def basic_auth(username, password, required_scopes=None):
     if username == 'admin' and password == 'secret':
         return {'sub': 'admin'}
@@ -17,27 +18,28 @@ def basic_auth(username, password, required_scopes=None):
     return None
 
 
+app = connexion.FlaskApp(__name__, specification_dir=basedir)
+app.add_api('swagger.yml')
+
+# Get the underlying Flask app instance
+app_instance = app.app
+
+# Configure the SqlAlchemy part of the app instance
+app_instance.config["SQLALCHEMY_ECHO"] = False
+app_instance.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
+app_instance.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+# Debug
+app_instance.url_map.strict_slashes = False
+CORS(app_instance)
+
+# Create the SqlAlchemy db instance
+db = SQLAlchemy(app_instance)
+
+# Initialize Marshmallow
+ma = Marshmallow(app_instance)
+
+
 if __name__ == '__main__':
-    app = connexion.FlaskApp(__name__, specification_dir=basedir)
-    app.add_api('swagger.yml')
-
-    # Get the underlying Flask app instance
-    app_instance = app.app
-
-    # Configure the SqlAlchemy part of the app instance
-    app_instance.config["SQLALCHEMY_ECHO"] = False
-    app_instance.config["SQLALCHEMY_DATABASE_URI"] = sqlite_url
-    app_instance.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    # Debug
-    app_instance.url_map.strict_slashes = False
-    CORS(app_instance)
-
-    # Create the SqlAlchemy db instance
-    db = SQLAlchemy(app_instance)
-
-    # Initialize Marshmallow
-    ma = Marshmallow(app_instance)
-
     app.run(port=5000)
     #app.run(port=5000, ssl_context='adhoc')
